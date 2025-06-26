701b465c3851e7b21a8e880fc890f46ca30697e1
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-06-26 16:56:09 -0600

    introduccion a los loops mediante do, while, for y foreach

--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,48 @@
 <?php include 'includes/header.php';
 
+// While
+$i = 0;
 
+while($i < 10){
+
+    echo $i . "<br>";
+
+    $i++; // Incremento
+}
+
+echo "<br>";
+
+// Do While
+$i = 0;
+do {
+    echo $i . "<br>";
+    $i++;
+} while ($i < 10);
+
+for($i = 0; $i < 10; $i++){
+    echo $i . "<br>";
+}
+
+// For each
+$clientes = array('Pedro', 'Juan', 'Karen'); // tipo de array 1
+
+// foreach ($clientes as $cliente){
+//     echo $cliente . "<br>";
+// }
+
+foreach($clientes as $cliente):
+    echo $cliente . "<br>";
+endforeach;
+
+$cliente = [ // Tipo de Array 2
+    'nombre' => 'Rodolfo',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+foreach ($cliente as $key => $valor):
+    echo $valor . "<br>";
+endforeach; 
 
 
 include 'includes/footer.php';

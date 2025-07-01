81566f93ef6efb1288d4f853ddac7b4a53d2f80e
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-07-01 13:44:13 -0600

    Manejo de retornos mediante tipos

--- 16-types.php
+++ 16-types.php
@@ -1,7 +1,18 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado) : string {
+    if($autenticado) {
+        return "El usuario está autenticado";
+    } else {
+        return "El usuario no está autenticado";
+    }
+}
 
+$usuario = usuarioAutenticado(true);
 
+echo $usuario;
 
 
 include 'includes/footer.php';
